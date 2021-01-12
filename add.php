725348808c34b2776efef62659f60636033bd74a
Arunachalam Muthupalaniappan <arun.ny@outlook.com>
2021-01-12 00:45:16 -0500

    Update add.php

--- add.php
+++ add.php
@@ -5,16 +5,11 @@ ini_set('display_errors', 1);
    	include("connect.php");
    	
    	$link=Connection();
-   	// $ts  = strtotime("now -2 hours");
-    // $ts1 = date("Y-m-d h:m:s",$ts);
 
 	$temp1=$_POST["temp1"];
 	$hum1=$_POST["hum1"];
 	$devid=$_POST["devid"];
 
-// 	$query = "INSERT INTO `izilla_tlogs` (`temp`, `hum`, `device_id`,`timestamp`) 
-// 		VALUES ('".$temp1."','".$hum1."','".$devid."', '".$ts1."')"; 
-   	
    	$query = "INSERT INTO `izilla_tlogs` (`device_id`, `temp`, `hum`) 
 		VALUES ('".$devid."','".$temp1."','".$hum1."')"; 
    	
